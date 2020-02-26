763a55b6c2848734cadfb2def1799e7cdbffea8a
unknown <onew1ng3d@gmail.com>
2020-02-26 14:54:44 -0500

    netlify header added

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,9 +1,10 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: process.env.NODE_ENV === 'development'
-    ? 'http://localhost:9000/.netlify/functions/graphql'
-    : 'https://mycl-server.netlify.com/.netlify/functions/graphql',
+  // uri: process.env.NODE_ENV === 'development'
+  //   ? 'http://localhost:9000/.netlify/functions/graphql'
+  //   : 'https://mycl-server.netlify.com/.netlify/functions/graphql',
+  uri: 'https://mycl-server.netlify.com/.netlify/functions/graphql',
   request: async operation => {
     const token = window.localStorage.getItem('token');
     operation.setContext({
