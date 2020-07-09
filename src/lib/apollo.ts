1eb1f1d8ab9efbc290061a1281857a1c287ac0cd
unknown <onew1ng3d@gmail.com>
2020-07-09 15:50:57 -0400

    changed prod uri for server

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -4,7 +4,7 @@ import { NormalizedCacheObject } from 'apollo-cache-inmemory';
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   uri: process.env.NODE_ENV === 'development'
     ? 'http://localhost:9000/.netlify/functions/graphql'
-    : 'https://myiu-ecommerce-server.netlify.com/.netlify/functions/graphql',
+    : 'https://myiu-ecommerce-server.netlify.app/.netlify/functions/graphql',
   request: async operation => {
     const token = window.localStorage.getItem('token');
     operation.setContext({
